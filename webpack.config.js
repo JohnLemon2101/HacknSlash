7081643354097781e6bfa5c9b9aa5df9e5b99350
CyrilGoldenschue <cyril.goldenschue@emploilausanne.ch>
2023-09-21 15:19:31 +0200

    Use webpack to fuse any js file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = { 
+  mode: 'development', // Vous pouvez également utiliser 'production' ou 'none' en fonction de vos besoins.
+  entry: {
+    bundle: [
+        path.resolve(__dirname, 'assets/js/player.js'),
+        path.resolve(__dirname, 'assets/js/projectile.js'),
+    ]
+    
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, "."),
+  },
+};

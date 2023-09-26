6876438ba37530e085fff2202cb0c7aebbd8bf46
CyrilGoldenschue <cyril.goldenschue@emploilausanne.ch>
2023-09-26 16:27:56 +0200

    Global variable

---
+++ assets/js/data.js
@@ -0,0 +1,13 @@
+// taille de la map
+export var windowWidth = window.innerWidth;
+export var windowHeight = window.innerHeight;
+
+// information du joueur
+export var playerWidth = 20;
+export var playerHeight = 50;
+export var speedY = 5; // Vitesse de déplacement
+export var speedX = (speedY * windowWidth) / playerWidth;
+
+//info monstre
+export const monsterHeight = 50;
+export const monsterWidth = 50;

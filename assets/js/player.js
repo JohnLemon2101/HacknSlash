f49a7557d0352ff1881b3a11beb2ffb07a8861e5
CyrilGoldenschue <cyril.goldenschue@emploilausanne.ch>
2023-09-26 09:49:26 +0200

    use import format

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,65 +1,65 @@
 import { startShooting } from "./projectile";
 
-document.addEventListener("DOMContentLoaded", function() {
-    var persoHitbox = document.getElementById("perso");
-    var map = document.getElementById("game");
+var speed = 5; // Vitesse de déplacement
+var life = 2;
+var damage = 1;
 
-    var playerHitbox = persoHitbox.getBoundingClientRect();
+const playerWidth = 20;
+const playerHeight = 50;
 
-    const playerHeight = 50;
-    const playerWidth = 20;
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+var map = document.getElementById("game");
 
-    var speed = 5; // Vitesse de déplacement
-    var life = 2;
-    
-    // Récupérez la taille de la fenêtre
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+export function createPlayer() {
+    const player = document.createElement("div");
+    const imagePlayer = document.createElement("img");
+    player.id = "player";
+    imagePlayer.src = "./assets/images/img.png";
+    document.getElementById("game").appendChild(player);
+    player.appendChild(imagePlayer);
 
-    var positionX = windowWidth / 2; // Position initiale de l'image en X
-    var positionY = windowHeight / 2; // Position initiale de l'image en Y
-    
-    var targetX = positionX; // Position cible en X
-    var targetY = positionY; // Position cible en Y
+    // Définir la taille et la position initiale du joueur
+    player.style.width = playerWidth + "px";
+    player.style.height = playerHeight + "px";
 
-    persoHitbox.style.top = positionY + "px";
-    persoHitbox.style.left = positionX + "px";
+    // Position initiale du joueur (au centre de la fenêtre)
 
-    persoHitbox.style.width = playerWidth + "px";
-    persoHitbox.style.height = playerHeight + "px";
+    const initialX = (windowWidth - playerWidth) / 2;
+    const initialY = (windowHeight - playerHeight) / 2;
 
-    map.style.width = windowWidth + "px";
-    map.style.height = windowHeight + "px";
+    player.style.left = initialX + "px";
+    player.style.top = initialY + "px";
+}
 
-    function movePlayer() {
-        const stepX = (targetX - positionX);
-        const stepY = (targetY - positionY);
-        positionX += stepX;
-        positionY += stepY;
 
-        persoHitbox.style.top = positionY + "px";
-        persoHitbox.style.left = positionX + "px";
+    function handlePlayerMovement(event, player) {
+        const playerRect = player.getBoundingClientRect();
 
-        if (Math.abs(targetX - positionX) > 1 || Math.abs(targetY - positionY) > 1) {
-            requestAnimationFrame(movePlayer);
-        }
+        var targetX = playerRect.left; // Position cible en X
+        var targetY = playerRect.top; // Position cible en Y
+
+        if(event.key === "w" && targetY > 0) { targetY -= speed; }
+        if(event.key === "s" && targetY < windowHeight - playerHeight) { targetY += speed; }
+        if(event.key === "a" && targetX > 0) { targetX -= speed; }
+        if(event.key === "d" && targetX < windowWidth - playerWidth) { targetX += speed; }
+
+
+        player.style.top = targetY + "px";
+        player.style.left = targetX + "px";
     }    
+    
     
     document.addEventListener("keydown", function(event) 
     {
-        if(event.key === "w" && targetY > 0) { targetY = positionY - speed; }
-        if(event.key === "a" && targetX > 0) { targetX = positionX - speed; }
-        if(event.key === "s" && targetY < windowHeight - playerHeight) { targetY = positionY + speed; }
-        if(event.key === "d" && targetX < windowWidth - playerWidth) { targetX = positionX + speed; }
-
-        requestAnimationFrame(movePlayer);
+        handlePlayerMovement(event, player);
     });
 
     // Gestionnaire d'événement pour déclencher le tir (par exemple, un clic de souris)
     map.addEventListener("mousedown", function(event) {
-        startShooting(event.clientX, event.clientY);
+        startShooting(event.clientX, event.clientY, player, damage);
     });
-});
+
 
 
 

9d25f627f01a7bbcdf95271cacb23fdbd9d68c24
CyrilGoldenschue <cyril.goldenschue@emploilausanne.ch>
2023-09-27 09:31:35 +0200

    add hp bar

--- assets/js/player.js
+++ assets/js/player.js
@@ -4,24 +4,47 @@ import { windowHeight, windowWidth, playerHeight, playerWidth} from './data.js';
 var map = document.getElementById("map");
 var game = document.getElementById("game");
 
-export var life = 1;
+export var life = 3;
 var damage = 1;
 
 export function createPlayer() {
     const player = document.createElement("div");
     player.dataset.life = life;
+    player.dataset.maxLife = life;
     player.dataset.damage = damage;
     player.id = "player";
 
+
+    const hp = document.createElement("div");
+    hp.id = "hp"
+
+    hp.style.width = 20 * player.dataset.life + "px";
+
+    for(let i = 0; i < player.dataset.life; i++){
+        
+        const imageHeart = document.createElement("img");
+        imageHeart.src = "./assets/images/full_heart.png";
+        imageHeart.className = "heart";
+        imageHeart.id = "heart" + i;
+        hp.appendChild(imageHeart);
+    }
+    
     const imagePlayer = document.createElement("img");
     imagePlayer.src = "./assets/images/img.png";
 
     map.appendChild(player);
-    player.appendChild(imagePlayer);
+
+    
+    map.appendChild(hp);
+    
 
     // Définir la taille et la position initiale du joueur
     player.style.width = playerWidth + "px";
     player.style.height = playerHeight + "px";
+    imagePlayer.style.width = playerWidth + "px";
+    imagePlayer.style.height = playerHeight + "px";
+
+    player.appendChild(imagePlayer);
 
     // Position initiale du joueur (au centre de la fenêtre)
 
